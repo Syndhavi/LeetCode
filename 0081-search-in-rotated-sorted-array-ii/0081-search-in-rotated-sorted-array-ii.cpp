7c276c4fbfde5047e8561989defba6fff65f57cb
Syndhavi <syndhavisenthil@gmail.com>
2026-05-30 18:53:20 +0530

    Time: 0 ms (100%), Space: 18.1 MB (33.78%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -10,11 +10,11 @@ public:
                 low++;high--;continue;
             }
             if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]) high=mid-1;
+                if(nums[low]<=target && target<nums[mid]) high=mid-1;
                 else low=mid+1;
             }
             else{
-                if(nums[mid]<=target && target<=nums[high]) low=mid+1;
+                if(nums[mid]<target && target<=nums[high]) low=mid+1;
                 else high=mid-1;
             }
         }
